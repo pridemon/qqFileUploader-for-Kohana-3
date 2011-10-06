25d4a64ebca517d07db740c0f385d41abef27ad2
Ronni Egeriis Persson <ronni@egeriis.me>
2011-10-06 23:31:06 +0200

    Added documentation to main class Fileupload

--- classes/fileupload.php
+++ classes/fileupload.php
@@ -1,9 +1,24 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+/**
+ * A module to easily integrate Andrew Valums' AJAX enabled file uploader in Kohana 3
+ *
+ * {@link https://github.com/egeriis/qqFileUploader-for-Kohana-3}
+ *
+ * @author Ronni Egeriis Persson {@link http://egeriis.me}
+ */
 class Fileupload
 {
+	/**
+	 * The name through which the file upload is sent.
+	 */
 	public static $post_name = 'qqfile';
 	
+	/**
+	 * Checks wheather a file upload exists through either AJAX or traditional upload.
+	 *
+	 * @return boolean
+	 */
 	public static function exists()
 	{
 		if (qq_Ajax::exists()) return true;
@@ -11,6 +26,11 @@ class Fileupload
 		return false;
 	}
 	
+	/**
+	 * Get file name of the uploaded file. Returns null if no file upload is found.
+	 *
+	 * @return string
+	 */
 	public static function name()
 	{
 		if (qq_Ajax::exists())
@@ -25,6 +45,11 @@ class Fileupload
 		return null;
 	}
 	
+	/**
+	 * Get file size of uploaded file. Returns 0 if no file upload is found.
+	 *
+	 * @return int
+	 */
 	public static function size()
 	{
 		if (qq_Ajax::exists())
@@ -39,6 +64,13 @@ class Fileupload
 		return 0;
 	}
 	
+	/**
+	 * Saves the uploaded file to specified path.
+	 *
+	 * @param string $path
+	 * @param int $chmod (default: 0644)
+	 * @return boolean
+	 */
 	public static function save($path=null, $chmod=0644)
 	{
 		$exists = false;
